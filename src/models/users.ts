426e2bc5f4007399b375f068e4c63ce10cec79aa
Avinash <p4avinashmaurya@gmail.com>
2024-08-11 11:20:19 +0530

    improved folder structure

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,67 +1,59 @@
 import { model, Schema, Document, Types } from "mongoose"
-
-interface IUser extends Document {
-    lastName: string;
-    firstName: string;
-    email: string;
-    password: string;
-    bio?: string;
-    dp?: string;
-    followers: Types.ObjectId[];
-    following: Types.ObjectId[];
-    createdAt: Date;
-    lastActive: Date;
-}
+import { IUser } from "../types/users.type"
 
 const userSchema = new Schema<IUser>({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    default: "",
+    trim: true,
+  },
+  dp: {
+    type: String,
+    default: "", // TBD - You can modify this later based on the implementation
+  },
+  followers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
+  ],
+  following: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    bio: {
-        type: String,
-        default: '',
-        trim: true,
-    },
-    dp: {
-        type: String,
-        default: '',  // TBD - You can modify this later based on the implementation
-    },
-    followers: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    }],
-    following: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    lastActive: {
-        type: Date,
-        default: Date.now,
-    },
-});
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  lastActive: {
+    type: Date,
+    default: Date.now,
+  },
+})
 
-const User = model<IUser>('User', userSchema);
+const User = model<IUser>("User", userSchema)
 
-export default User;
+export default User
