71537c128ff5e9f10d4c632d9b557427b169b12f
Ayush <rajanand.ayush98.com>
2024-08-11 12:38:55 +0530

    added request validator for registration

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { register } from '../controllers/users';
+import { validateUserRegistration, handleValidationErrors } from '../validators/user.validator';
 
 const router = express.Router();
 
-router.post('/register', register)
+router.post('/register',validateUserRegistration, handleValidationErrors,  register)
 
 export default router;
